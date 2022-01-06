d34835a722231bef193bba4eae949b1376f5793c
Martin Pham <martinp0512@gmail.com>
2022-01-06 00:37:05 +0000

    use SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,13 +8,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const queryString= `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 JOIN teachers ON teachers.id = teacher_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-ORDER BY teacher;`).then(res => {
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;`
+
+const values= [`%${process.argv[2]}%`];
+
+pool.query(queryString, values)
+.then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
